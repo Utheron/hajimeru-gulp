fa3581161ff3a1e6f3175ed4d59fb273576add89
Utheron <git@utheron.com>
2020-02-01 19:04:18 +0100

    upd 2020.0201

--- gulpfile.js
+++ gulpfile.js
@@ -51,12 +51,12 @@ const browserSync       = require('browser-sync').create();
 // #####################################################################
 const siteURL           = process.env.SITE_URL;
 const externalURL       = process.env.EXTERNAL_URL;
-const assetsURL         = process.env.DIST + process.env.TPL;
-const assetsSRC         = process.env.SRC + process.env.TPL;
+const assetsURL         = process.env.DIST;
+const assetsSRC         = process.env.SRC;
 const imageFolder       = process.env.IMG;
 
-
-const styleSRC          = [ assetsSRC + '/scss/style.scss' ]
+const styleSRC          = [assetsSRC + '/scss/style.scss']
+const styleClean        = [assetsSRC + '/scss/clean.scss']
 const styleWatch        = assetsSRC + '/scss/**/*.scss';
 
 const scriptSRC         = 'script.js';
@@ -75,9 +75,7 @@ const htmlFiles          = '**/*.html';
 // #####################################################################
 // # SYMFONY
 // #####################################################################
-// const phpFiles          = '**/*.php';
-// const htmlFiles         = '../templates/**/*.html';
-// const twigFiles         = '../templates/**/*.twig';
+const twigFiles         = '../templates/**/*.twig';
 
 // #####################################################################
 // # FUNCTIONS
@@ -98,52 +96,72 @@ function reload(done) {
     done();
 }
 
-// TODO: Create a standalone purify function
+function styles_purify(done) {
+    src(styleSRC, {allowEmpty: true})
+    .pipe(purify(['./**/*.html']))      // Your view files
+    .pipe(replace('!important', ''))    // Remove !important for AMP Validator
+    .pipe(rename('clean.scss'))
+    .pipe(dest(assetsSRC + '/scss'));
+    done();
+}
 
 function styles(done) {
-    src(styleSRC, {allowEmpty: true})
+    var fs = require('fs');
+    if (fs.existsSync('src/scss/clean.scss')) {
+        src(styleClean, {allowEmpty: true})
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(sass({errLogToConsole: true, outputStyle: 'compressed'}))
+        .pipe(autoprefixer({Browserlist: ['last 2 versions', '> 5%', 'Firefox ESR']}))
         .on('error', console.error.bind(console))
-        .pipe(autoprefixer({browsers: ['last 2 versions', '> 5%', 'Firefox ESR']}))
-        .pipe(purify(['./HTML/**/*.html']))     // Your view files
         .pipe(sass({errLogToConsole: true, outputStyle: 'compressed'}))
-        .pipe(replace('!important', ''))        // Remove !important for AMP Validator
-        .pipe(rename({extname: '.min.css'}))
+        .pipe(rename('style.min.css'))
         .pipe(sourcemaps.write('./'))
         .pipe(dest(assetsURL + '/css'))
         .pipe(browserSync.stream());
+        done();
+    } else {
+        src(styleSRC, {allowEmpty: true})
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({Browserlist: ['last 2 versions', '> 5%', 'Firefox ESR']}))
+        .on('error', console.error.bind(console))
+        .pipe(sass({errLogToConsole: true, outputStyle: 'compressed'}))
+        .pipe(rename('style.min.css'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(dest(assetsURL + '/css'))
+        .pipe(browserSync.stream());
+        done();
+    }
     done();
 }
 
 function scripts(done) {
     scriptFiles.map( function( entry ) {
         return browserify({entries: [scriptFolder + entry]})
-            .transform(babelify, {presets: ['@babel/preset-env']})
-            .bundle()
-            .pipe(source(entry))
-            .pipe(rename({extname: '.min.js'}))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(uglify())
-            .pipe(sourcemaps.write('./'))
-            .pipe(dest(assetsURL + '/js'))
-            .pipe(browserSync.stream());
+        .transform(babelify, {presets: ['@babel/preset-env']})
+        .bundle()
+        .pipe(source(entry))
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(dest(assetsURL + '/js'))
+        .pipe(browserSync.stream());
     });
     done();
 }
 
 function compress_img() {
     return src(imageSRC)
-        .pipe(cache(imagemin([
-            imageminPngquant({speed: 1, quality: 98}),
-            imageminGiflossy({optimizationLevel: 3, optimize: 3, lossy: 2}),
-            imagemin.svgo({plugins: [{ removeViewBox: false }]}),
-            imagemin.jpegtran({ progressive: true }),
-            imageminMozjpeg({ quality: 70 })
-        ])))
-        .pipe(dest(assetsURL + imageFolder));
+    .pipe(cache(imagemin([
+        imageminPngquant({speed: 1, quality: 98}),
+        imageminGiflossy({optimizationLevel: 3, optimize: 3, lossy: 2}),
+        imagemin.svgo({plugins: [{ removeViewBox: false }]}),
+        imagemin.jpegtran({ progressive: true }),
+        imageminMozjpeg({ quality: 70 })
+    ])))
+    .pipe(dest(assetsURL + imageFolder));
 }
 
 function watch_files() {
@@ -152,12 +170,13 @@ function watch_files() {
     watch(phpFiles, reload);
     watch(htmlFiles, reload);
     // SYMFONY
-    // watch(twigFiles, reload);
+    watch(twigFiles, reload);
 }
 
 // #####################################################################
 // # GULP TASKS
 // #####################################################################
 task('default', parallel(styles, scripts));
+task('purify', parallel(styles_purify));
 task('watch', parallel(browser_sync, watch_files));
 task('imagemin', parallel(compress_img));
